e6509b820f67b9cb3f2a6e1cb75df1973def61e3
Lucius Bono <luciusbono@gmail.com>
2017-08-31 14:26:24 -0700

    Added a rudimentary hash file check that will only install a package or execute a script if the hash is what we expect it to be (based on the manifest file).

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-import subprocess, glob, json, os
+import subprocess, glob, json, os, hashlib
 
 local_repo = 'repo'
 os.environ["repo"] = local_repo
@@ -13,14 +13,26 @@ def script_exec(script):
     out, err = pipes.communicate()
     print out.decode('utf-8'), err.decode('utf-8'), pipes.returncode
 
+def hash_file(filename):
+    hash = hashlib.sha256()
+    with open (filename, 'rb') as file:
+        for chunk in iter(lambda: file.read(4096), b""):
+            hash.update(chunk)
+    return hash.hexdigest() 
+
+
 with open ('manifest.json', 'r') as manifest_file:
     data = json.load(manifest_file)
     for item in data['packages']:
-        if ".pkg" in item['name']: 
-            print "Installing:", item['name']
-            pkg_install(item['path'])
-        if ".sh" in item['name']:
-            print "Executing:", item['name']
-            script_exec(item['path'])
-
-manifest_file.close()
+        hash = hash_file(item['path'])
+        if hash == item['hash']:
+            print "The hash: ", hash, " matches what we expect."
+            if ".pkg" in item['name']: 
+                print "Installing:", item['name']
+                pkg_install(item['path'])
+            if ".sh" in item['name']:
+                print "Executing:", item['name']
+                script_exec(item['path'])
+        else: 
+            print "The hash: ", hash, " does not match the hash in the manifest: ", item['hash']
+    manifest_file.close()

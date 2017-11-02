cdcc855768348b0892ecffbf265b91203371cab7
Lucius Bono <luciusbono@gmail.com>
2017-11-02 12:17:15 -0700

    Fleshed out dock config

--- repo/dock-config.sh
+++ repo/dock-config.sh
@@ -2,7 +2,9 @@
 
 dockutil="$(curl -fsSL https://raw.githubusercontent.com/kcrawford/dockutil/b7fcec8aae863cd83cb27c2cf8bd3e739ece0795/scripts/dockutil)"
 
-python -c "$dockutil" --add "/Applications/Firefox.app" --after Launchpad
-python -c "$dockutil" --add "/Applications/VidyoDesktop.app" --after Firefox
-python -c "$dockutil" --add "/Applications/Crashplan" --after VidyoDesktop
-python -c "$dockutil" --list
+python -c "$dockutil" --remove all --no-restart
+python -c "$dockutil" --add "/Applications/Launchpad.app" --position beginning --no-restart
+python -c "$dockutil" --add "/Applications/Firefox.app" --after Launchpad --no-restart
+python -c "$dockutil" --add "/Applications/VidyoDesktop.app" --after Firefox --no-restart
+python -c "$dockutil" --add "/Applications/Crashplan.app" --after VidyoDesktop --no-restart
+python -c "$dockutil" --add "/Applications/System Preferences.app" --position end

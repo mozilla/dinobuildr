2d4d2f775d288d4a2c47d639c9e983f177a14bc2
Lucius Bono <luciusbono@gmail.com>
2017-11-06 13:31:58 -0800

    testing wallpaper environment variable

--- repo/wallpaper.sh
+++ repo/wallpaper.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+echo $DINOPATH
 echo "We're setting the dang wallpaper now."
 osascript -e 'tell application "Finder" to set desktop picture to POSIX file "/Users/Shared/dino-wallpaper.png"'

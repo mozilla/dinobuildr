e3d53098fb98e7d5fe3318e7a9baff3c26a7f854
Lucius Bono <luciusbono@gmail.com>
2017-11-16 21:31:38 -0800

    Adding MacOS update.

---
+++ repo/update-macos.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+# this script gets software updates from Apple. As you can see it's real complicated.  
+
+softwareupdate --install --all

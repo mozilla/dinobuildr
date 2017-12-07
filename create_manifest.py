6e6ce69cb908c514fde2939a7267e2cfe26ffe60
Lucius Bono <lbono@mozilla.com>
2017-12-07 10:31:16 -0800

    Testing complete, switching branches back to master for push to production.

--- create_manifest.py
+++ create_manifest.py
@@ -19,9 +19,9 @@ from collections import OrderedDict
 # branch - the branch that we are using. useful to change this if developing /
 # testing
 local_dir = 'build-temp'
-org = "ctbfourone"
+org = "mozilla"
 repo = "dinobuildr"
-branch = "ctbird"
+branch = "master"
 
 # lfs_url - the generic LFS url structure that github uses
 # raw_url -  the generic RAW url structure that github uses

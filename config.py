cc67f9d248cd675c9663e551ba00e3095bb2e646
Lucius Bono <lbono@mozilla.com>
2018-05-30 14:31:32 -0700

    Moving scripts to the corisca subdirectory

--- config.py
+++ config.py
@@ -82,7 +82,7 @@ raw_url = "https://raw.githubusercontent.com/%s/%s/%s/" % (org, repo, branch)
 manifest_url = "https://raw.githubusercontent.com/%s/%s/%s/%s" % (org, repo, branch, manifest)
 manifest_file = "%s/%s" % (local_dir, manifest)
 default_manifest_hash = "61f6fc9b2bf9f2711c9eb4e2e9032dc825534fba83b02db0f1fcda09fb3fbdb5"
-ambient_manifest_hash = "e52ffe56afa0175eea466123c0ecb6fbf82b4ed03f8e2e0b96738ea07cff9510"
+ambient_manifest_hash = "b5adb6718bd08802692635049ec6541e9f67c97a4983c13a3f1758e3a44919b3"
 manifest_hash = default_manifest_hash
 if manifest == "ambient_manifest.json":
     manifest_hash = ambient_manifest_hash

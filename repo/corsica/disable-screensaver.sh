cca72d22d94c46a07ee8f12e8ff86007dc164d01
Tristan Thomas <tthomas@mozilla.com>
2018-05-30 16:42:03 -0700

    This script can now run before the corsica user is created

--- repo/corsica/disable-screensaver.sh
+++ repo/corsica/disable-screensaver.sh
@@ -4,6 +4,11 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+# Credit for most of this code goes to barnesaw from the JAMF Nation forums.
 # This script disables the screensaver under the corsica user account.
 
-sudo -u corsica defaults -currentHost write com.apple.screensaver idleTime 0
+MAC_UUID=$(system_profiler SPHardwareDataType | awk -F" " '/UUID/{print $3}')
+
+for USER_TEMPLATE in "/System/Library/User Template"/*; do
+	/usr/bin/defaults write "${USER_TEMPLATE}"/Library/Preferences/ByHost/com.apple.screensaver.$MAC_UUID "idleTime" -int 0
+done

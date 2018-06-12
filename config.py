0d49dbb851885e41cbb5c6d9dc7bdb15d8052652
Tristan Thomas <tthomas@mozilla.com>
2018-06-12 15:18:34 -0700

    Added configuration profile to skip the Siri Setup Assistant screen

--- config.py
+++ config.py
@@ -82,7 +82,7 @@ raw_url = "https://raw.githubusercontent.com/%s/%s/%s/" % (org, repo, branch)
 manifest_url = "https://raw.githubusercontent.com/%s/%s/%s/%s" % (org, repo, branch, manifest)
 manifest_file = "%s/%s" % (local_dir, manifest)
 default_manifest_hash = "61f6fc9b2bf9f2711c9eb4e2e9032dc825534fba83b02db0f1fcda09fb3fbdb5"
-ambient_manifest_hash = "03cb845f8ed1dcaf1bb1f1fce6e6c2b367afe4e1df79accdba363c4dfdfae058"
+ambient_manifest_hash = "ceb7a239b3eb65e4917019838c3489681a79fbaf70a524c4a2b88d30eb499cce"
 manifest_hash = default_manifest_hash
 if manifest == "ambient_manifest.json":
     manifest_hash = ambient_manifest_hash

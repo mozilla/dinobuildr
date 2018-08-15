71e47d5401804ba5db4bc320d85d3124eb74a224
Lucius Bono <lbono@mozilla.com>
2018-08-14 18:37:41 -0700

    re-writing auto-update mechanism to check against an external file to see if it's okay to get updates.

--- repo/corsica/auto-update.sh
+++ repo/corsica/auto-update.sh
@@ -1,28 +1,70 @@
 #!/bin/bash
 
-cat > /Library/LaunchDaemons/update-ff-macos.plist <<- "EOF"
-<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-<plist version="1.0">
-<dict>
-    <key>Label</key>
-    <string>update-firefox-macos</string>
-    <key>ProgramArguments</key>
-    <array>
-        <string>bash</string>
-        <string>-c</string>
-        <string>softwareupdate -ia;
-        shutdown -r now</string>
-    </array>
-    <key>StartCalendarInterval</key>
-    <dict>
-        <key>Hour</key>
-        <integer>3</integer>
-        <key>Minute</key>
-        <integer>00</integer>
-    </dict>
-</dict>
-</plist>
-EOF
+cat > /usr/local/auto-update-routine.sh <<- "EOF"
+	#!/bin/bash
+	if [ "$(curl https://raw.githubusercontent.com/luciusbono/informant/master/informant.txt)" = "greenlight" ]; then
+	    softwareupdate -ia
+	fi
 
+	shutdown -r now
+	EOF
+
+chmod +x /usr/local/auto-update-routine.sh
+
+if [ "$1" = "canary" ]; then
+	cat > /Library/LaunchDaemons/update-ff-macos.plist <<- "EOF"
+		<?xml version="1.0" encoding="UTF-8"?>
+		<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+		<plist version="1.0">
+		<dict>
+		    <key>Label</key>
+		    <string>update-firefox-macos</string>
+		    <key>Program</key>
+		    <string>/usr/local/auto-update-routine.sh</string>
+		    <key>StartCalendarInterval</key>
+		    <dict>
+		        <key>Hour</key>
+		        <integer>3</integer>
+		        <key>Minute</key>
+		        <integer>00</integer>
+		    </dict>
+		</dict>
+		</plist>
+		EOF
+
+else
+	cat > /Library/LaunchDaemons/update-ff-macos.plist <<- "EOF"
+		<?xml version="1.0" encoding="UTF-8"?>
+		<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+		<plist version="1.0">
+		<dict>
+		    <key>Label</key>
+		    <string>update-firefox-macos</string>
+		    <key>Program</key>
+		    <string>/usr/local/auto-update-routine.sh</string>
+		    <key>StartCalendarInterval</key>
+		    <array>
+		        <dict>
+		            <key>Day</key>
+		            <integer>1</integer>
+		            <key>Hour</key>
+		            <integer>3</integer>
+		            <key>Minute</key>
+		            <integer>00</integer>
+		        </dict>
+		        <dict>
+		            <key>Day</key>
+		            <integer>15</integer>
+		            <key>Hour</key>
+		            <integer>3</integer>
+		            <key>Minute</key>
+		            <integer>00</integer>
+		        </dict>
+		    </array>
+		</dict>
+		</plist>
+		EOF
+fi
+
+launchctl unload /Library/LaunchDaemons/update-ff-macos.plist
 launchctl load /Library/LaunchDaemons/update-ff-macos.plist

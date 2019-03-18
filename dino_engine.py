c1d77da17fdaddbb0b116b0ffd5124ad42ea2fb5
Vanessa White <vwhite@mozilla.com>
2019-03-18 15:09:45 -0700

    updating crashplan to 6.9.2

--- dino_engine.py
+++ dino_engine.py
@@ -83,7 +83,7 @@ lfs_url = "https://github.com/%s/%s.git/info/lfs/objects/batch" % (org, repo)
 raw_url = "https://raw.githubusercontent.com/%s/%s/%s/" % (org, repo, branch)
 manifest_url = "https://raw.githubusercontent.com/%s/%s/%s/%s" % (org, repo, branch, manifest)
 manifest_file = "%s/%s" % (local_dir, manifest)
-default_manifest_hash = "57d5f588ae6fed4b712d396f46a1b0865fc0e9e7c64ef6bc673e345643971148"
+default_manifest_hash = "9fb7796c003d2b858a115c22793ab84d41b0464eaab96fbf1cd3a8f166acd04d"
 ambient_display_manifest_hash = "d291d6dd00a69490798970a40dff9e13340455db6b3711c843e90a5f48733772"
 manifest_hash = default_manifest_hash
 

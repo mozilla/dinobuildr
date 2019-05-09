8b153de2280dc07b740bf12c735c744344056421
Lucius Bono <lbono@mozilla.com>
2019-05-08 17:33:19 -0700

    Switching back hardcoded url to dynamic-ish Zoom URL.

--- dino_engine.py
+++ dino_engine.py
@@ -83,7 +83,7 @@ lfs_url = "https://github.com/%s/%s.git/info/lfs/objects/batch" % (org, repo)
 raw_url = "https://raw.githubusercontent.com/%s/%s/%s/" % (org, repo, branch)
 manifest_url = "https://raw.githubusercontent.com/%s/%s/%s/%s" % (org, repo, branch, manifest)
 manifest_file = "%s/%s" % (local_dir, manifest)
-default_manifest_hash = "4cf576ef390f5fee38217d434a67f81600bb6d7e333477f078556f6082e3f413"
+default_manifest_hash = "29a75b8d91ea40e5660f1449f71522e7af1ff6f32488c846942f0af83b09f390"
 ambient_display_manifest_hash = "d291d6dd00a69490798970a40dff9e13340455db6b3711c843e90a5f48733772"
 manifest_hash = default_manifest_hash
 

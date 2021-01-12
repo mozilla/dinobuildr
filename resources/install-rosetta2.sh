2c31229fd64bfdd1064c6c034fd029896674ce5b
Jonathan Lin <jlin@winteroftext.ca>
2021-01-12 14:32:10 -0500

    create install-rosetta2.sh
    create install-rosetta2.sh

---
+++ resources/install-rosetta2.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+# this script checks for machine processor architecture and
+# installs rosetta 2 for arm systems
+
+processor_architecture=$(uname -p)
+if [[ "$processor_architecture" = 'arm' ]]; then
+    echo "Installing Rosetta 2 for Arm system"
+    /usr/sbin/softwareupdate --install-rosetta --agree-to-license
+    exit 0
+fi

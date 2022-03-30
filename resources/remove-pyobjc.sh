0d204d0e8c05f8dacf5f8942e5d7959a71169ac4
Jonathan Lin <jlin@winteroftext.ca>
2022-03-30 11:17:39 -0400

    remove-pyobjc

---
+++ resources/remove-pyobjc.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+# remove pyobjc-framework-SystemConfigruation package
+
+pip3 uninstall -y pyobjc-framework-SystemConfiguration
+pip3 uninstall -y pyobjc-core
+pip3 uninstall -y pyobjc-framework-Cocoa

6ee51b283f69f9a6955d4de6d06379f25b70e278
Jonathan Lin <jlin@winteroftext.ca>
2022-03-31 18:37:01 -0400

    remove command line tools too
    remove command line tools too

--- resources/remove-pyobjc.sh
+++ resources/remove-pyobjc.sh
@@ -5,7 +5,10 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 # remove pyobjc-framework-SystemConfigruation package
+# remove Command line Tools, the user can reinstall it (or xcode) if they want.
+# this is to make sure that we don't leave anything behind with dinobuildr
 
 pip3 uninstall -y pyobjc-framework-SystemConfiguration
 pip3 uninstall -y pyobjc-core
 pip3 uninstall -y pyobjc-framework-Cocoa
+rm -rf /Library/Developer/CommandLineTools
